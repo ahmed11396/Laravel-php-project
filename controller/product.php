392031f06d078b1d824fce34f51ee997e093dfd6
ahmed11396 <125726174+ahmed11396@users.noreply.github.com>
2024-04-05 07:54:39 +0500

    Add files via upload

---
+++ controller/product.php
@@ -0,0 +1,296 @@
+<?php
+
+
+class Product
+{
+
+    function __construct()
+    {
+
+    }
+    public function updateLedger($id,$name)
+    {
+        $sql = "update product_type set type_name = '$name' where id = $id";
+        $result = mysqli_query(mysqli_connect("localhost","root","","hstore"), $sql);
+        //$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+        if (isset($result))
+            return $result;
+        else
+        {
+            die("Database query not working. ");
+        }
+    }
+    public function getLedger($id)
+    {
+        $sql = "SELECT * from product_type where id = $id";
+        $result = mysqli_query(mysqli_connect("localhost","root","","hstore"), $sql);
+        $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+        if (isset($row))
+            return $row;
+        else
+        {
+            die("Database query not working. ");
+        }
+
+    }
+    public function deleteLedger($id)
+    {
+        $sql = "delete from product_type where id = $id";
+        $result = mysqli_query(mysqli_connect("localhost","root","","hstore"), $sql);
+        //$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+        if (isset($result))
+            return $result;
+        else
+        {
+            die("Database query not working. ");
+        }
+    }
+    public function getAllOrder()
+    {
+        $sql = "SELECT
+        `order_no`,
+        p_id,
+        products.p_name as pname,
+        `o_amount`,
+        `o_date`,
+        `status`,
+        products.p_amount as currentamount,
+
+        users.fname ,
+        users.lname,
+        `g_date`,
+        `g_amount`,
+        `after_amount`
+        FROM
+        `product_order`
+        INNER JOIN
+        orders
+        ON
+        product_order.order_no = orders.id
+        INNER JOIN products ON products.id = product_order.p_id
+        INNER JOIN users ON orders.user_id = users.id;";
+
+        $result = mysqli_query(mysqli_connect("localhost","root","","hstore"), $sql);
+
+
+        //test if there was a query error
+        if ($result)
+        return $result;
+        else
+        {
+            die("Database query not working. " . mysqli_error($connection));
+        }
+    }
+    public function approveOrder($order_id, $product_id,$g_amount,$c_amount)
+    {
+        $sql = "update orders set status = 'approved',g_date = current_date(),g_amount = $g_amount
+        where id = $order_id";
+        $result = mysqli_query(mysqli_connect("localhost","root","","hstore"), $sql);
+        if ($result)
+        echo "ok0";
+        else
+        {
+            die("Database update orders query not working. " . mysqli_error($connection));
+        }
+        $sql = "update products set p_amount = p_amount - $g_amount where
+        products.id = $product_id" ;
+        $result = mysqli_query(mysqli_connect("localhost","root","","hstore"), $sql);
+        if ($result)
+            echo "ok1";
+        else
+        {
+            die("Database update product query not working. " . mysqli_error($connection));
+        }
+        $sql = "update orders set after_amount = $c_amount - $g_amount where
+        orders.id = $order_id" ;
+        $result = mysqli_query(mysqli_connect("localhost","root","","hstore"), $sql);
+        if ($result)
+        echo "ok2";
+        else
+        {
+            die("Database update after amount orders query not working. " . mysqli_error($connection));
+        }
+    }
+    public function clearCart()
+    {
+        unset($_SESSION['cart']);
+        //header( "url=/orderproduct.php" );
+    }
+    public function getRecievedProductInLedger($id){
+        $sql = "SELECT
+        reciepts.r_date AS DATE,
+        product_for_store.before_amount AS b_amount,
+        product_for_store.amount AS recieved,
+        (
+            product_for_store.before_amount + product_for_store.amount
+        ) AS a_amount
+        FROM
+        product_for_store
+        INNER JOIN
+        products
+        ON
+        product_for_store.p_id = products.id
+        INNER JOIN
+        reciepts
+        ON
+        reciepts.id = product_for_store.r_id
+        WHERE
+        product_for_store.p_id = $id
+        ORDER BY
+        DATE ASC;";
+
+        $result = mysqli_query(mysqli_connect("localhost","root","","hstore"), $sql);
+
+        if ($result)
+        return $result;
+        else
+        {
+            die("Database query not working. " . mysqli_error($connection));
+        }
+    }
+    public function getProvidedProductInLedger($id){
+        $sql = "SELECT
+        orders.g_date AS DATE,
+         users.fname, users.lname,
+        orders.g_amount AS given,
+        orders.after_amount AS a_amount
+        FROM
+        product_order
+        INNER JOIN
+        products
+        ON
+        product_order.p_id = products.id
+        INNER JOIN
+        orders
+        ON
+        product_order.order_no = orders.id
+        INNER JOIN
+        users
+        ON
+        users.id = orders.user_id
+        WHERE
+        product_order.p_id = $id
+        ORDER BY
+        DATE ASC";
+
+        $result = mysqli_query(mysqli_connect("localhost","root","","hstore"), $sql);
+
+        if ($result)
+        return $result;
+        else
+        {
+            die("Database query not working. " . mysqli_error($connection));
+        }
+    }
+
+    public function selectLedger($id){
+        $sql = "SELECT * FROM `products` where ptype_id = $id;";
+
+        $result = mysqli_query(mysqli_connect("localhost","root","","hstore"), $sql);
+
+
+        //test if there was a query error
+        if ($result)
+        return $result;
+        else
+        {
+            die("Database query not working. " . mysqli_error($connection));
+        }
+    }
+    public function getAllLedger(){
+        $sql = "SELECT * FROM `product_type`;";
+
+        $result = mysqli_query(mysqli_connect("localhost","root","","hstore"), $sql);
+
+
+        //test if there was a query error
+        if ($result)
+        return $result;
+        else
+        {
+            die("Database query not working. " . mysqli_error($connection));
+        }
+    }
+    public function getAllManufacturer()
+    {
+        $sql = "SELECT `id`, `man_name` FROM `manufecturer`;";
+
+        $result = mysqli_query(mysqli_connect("localhost","root","","hstore"), $sql);
+
+
+        //test if there was a query error
+        if ($result)
+        return $result;
+        else
+        {
+            die("Database query not working. " . mysqli_error($connection));
+        }
+    }
+    public function getAllProductType()
+    {
+        $sql = "SELECT `id`, `type_name` FROM `product_type`;";
+
+        $result = mysqli_query(mysqli_connect("localhost","root","","hstore"), $sql);
+
+
+        //test if there was a query error
+        if ($result)
+        return $result;
+        else
+        {
+            die("Database query not working. " . mysqli_error($connection));
+        }
+    }
+    public function getAllProduct()
+    {
+        $sql = "SELECT products.id,p_name,p_amount,specs,man_name,type_name FROM products inner join
+        manufecturer on products.man_id = manufecturer.id inner join
+        product_type on products.ptype_id = product_type.id;";
+
+        $result = mysqli_query(mysqli_connect("localhost","root","","hstore"), $sql);
+
+
+        //test if there was a query error
+        if ($result)
+        return $result;
+        else
+        {
+            die("Database query not working. " );
+        }
+    }
+    public function getProduct($id)
+    {
+        $sql = "SELECT products.id,p_name,p_amount,specs,man_name,type_name FROM products inner join
+        manufecturer on products.man_id = manufecturer.id inner join
+        product_type on products.ptype_id = product_type.id where products.id = $id";
+
+        $result = mysqli_query(mysqli_connect("localhost","root","","hstore"), $sql);
+
+
+        //test if there was a query error
+        if ($result)
+        return $result;
+        else
+        {
+            die("Database query not working. " . mysqli_error());
+        }
+    }
+    public function getAllCashMemo()
+    {
+        $sql = "SELECT id,cmemo_no FROM reciepts;";
+
+        $result = mysqli_query(mysqli_connect("localhost","root","","hstore"), $sql);
+
+
+        //test if there was a query error
+        if ($result)
+        return $result;
+        else
+        {
+            die("Database query not working. " );
+        }
+    }
+}
+
+
+?>
---
+++ template/header.php
@@ -0,0 +1,112 @@
+<?php
+include_once 'controller/product.php';
+$_Product = new Product();
+ ?>
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ <link rel="stylesheet" href="resources/css/master.css">
+<div class="jumbotron">
+    <div class="text-center">
+        <h1>Khulna Medical College Store</h1>
+        <p>Store Management</p>
+    </div>
+</div>
+
+<nav class="navbar navbar-inverse">
+
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Khulna Medical College</a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="home.php"><span class="glyphicon glyphicon-home"></span> Home <span class="sr-only">(current)</span></a></li>
+
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown">Ledger Management
+                        <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a tabindex="-1" href="addledger.php">Add Ledger</a></li>
+                            <li class="dropdown-submenu">
+                                <a class="test" tabindex="-1" href="#">Ledger List <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <?php
+                                        $Result = $_Product->getAllLedger();
+                                        if(isset($Result)){
+                                        while($row=mysqli_fetch_array($Result,MYSQLI_ASSOC)){
+
+                                            ?>
+                                            <li><a tabindex="-1" href="ledger.php?select=<?php echo $row['id'] ?>"><?php echo $row['type_name'] ?></a></li>
+                                            <?php
+                                        }
+                                    }
+                                     ?>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown">Products
+                            <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a tabindex="-1" href="addproduct.php">Add Product</a></li>
+                                <li><a tabindex="-1" href="orderproduct.php">Order Product</a></li>
+                                <li><a tabindex="-1" href="productrecipt.php">Product For Store</a></li>
+                                <li><a tabindex="-1" href="approve_order.php">Approve Order</a></li>
+
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown">User Management
+                                <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a tabindex="-1" href="registration.php">Add User</a></li>
+                                    <li><a tabindex="-1" href="addrole.php">Manage Role</a></li>
+                                </ul>
+                            </li>
+
+
+                </ul>
+
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="nav navbar-nav navbar-right"><a href="#"><span class="glyphicon"></span> <?php echo $_SESSION['fullname'] ?> </a></li>
+                    <ul class="nav navbar-nav navbar-right">
+                        <?php
+                        if(!$_SESSION['email'])
+                        {
+                            ?>
+                            <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                            <?php
+                        }
+                        else {
+                            ?>
+                            <li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+                            <?php
+                        }
+                        ?>
+
+
+                    </ul>
+                </div>
+            </div>
+            <script>
+            $(document).ready(function(){
+                $('.dropdown-submenu a.test').on("click", function(e){
+                    $(this).next('ul').toggle();
+                    e.stopPropagation();
+                    e.preventDefault();
+                });
+            });
+            </script>
+
+        </nav>
